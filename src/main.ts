9c469c6cd9b44fe934b67305da36331553bd60b4
saysora <braden@devskull.com>
2023-12-14 01:47:50 -0800

    Begin setting up actual painting of different ui pieces

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import './facade.scss';
 
+const rootGet = getComputedStyle(document.body);
+const rootSet = document.documentElement;
+const debugBar = document.querySelector('.debug-bar');
+const pulldown = document.querySelector('.pull-down');
+
+if (pulldown) {
+  pulldown.addEventListener('click', () => {
+    if (debugBar) {
+      if (debugBar.classList.contains('in')) {
+        debugBar.classList.remove('in');
+      } else {
+        debugBar.classList.add('in');
+      }
+    }
+  });
+}
+
 const ui = document.querySelector(".ui");
 //const serverBar = document.querySelector(".server-bar");
 const serverButton = document.querySelector(".server-button");
@@ -12,3 +29,57 @@ serverButton?.addEventListener("click", () => {
   }
 });
 
+const getColor = (prop: string): string => {
+  return rootGet.getPropertyValue(prop);
+}
+
+const setColor = (prop: string, color: string): void => {
+  rootSet.style.setProperty(prop, `#${color.replace('#', '')}`);
+}
+
+const updateColorEv = (e: any, prop: string) => {
+  const val = e.target.value;
+  switch (true) {
+    case val.length < 3: {
+      return;
+    };
+    case val.length === 4: {
+    }
+    case val.length === 7: {
+      setColor(prop, e.target.value);
+    }
+    default: {
+      return;
+    }
+  }
+}
+
+const colorEditor = (prop: string, label: string): HTMLElement => {
+  const customProp = getColor(prop);
+
+  const div = document.createElement('div');
+  const input = document.createElement('input');
+
+  input.value = customProp;
+  input.maxLength = 7;
+  input.addEventListener('keyup', e => {
+    updateColorEv(e, prop);
+  });
+
+  div.innerHTML = `
+<label>${label.charAt(0) + label.slice(1)}</label>
+`;
+  div.appendChild(input);
+  return div;
+}
+
+window.addEventListener('load', () => {
+  if (debugBar) {
+    debugBar.appendChild(colorEditor('--server-bar-bg-color', 'Server BG'));
+    debugBar.appendChild(colorEditor('--group-bar-bg-color', 'Group BG'));
+    debugBar.appendChild(colorEditor('--channel-bar-bg-color', 'Channel BG'));
+    debugBar.appendChild(colorEditor('--main-header-bg-color', 'Header BG'));
+    debugBar.appendChild(colorEditor('--main-content-bg-color', 'Main BG'));
+    debugBar.appendChild(colorEditor('--main-panel-bg-color', 'Panel BG'));
+  }
+})
