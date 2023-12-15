a7e03f9aa928273d14e74a5c37b32dd992945647
saysora <braden@devskull.com>
2023-12-14 23:14:22 -0800

    Lotta progress, but still much more to do

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import './facade.scss';
+const version = "0.0.2";
+ 
 
 const rootGet = getComputedStyle(document.body);
 const rootSet = document.documentElement;
 const debugBar = document.querySelector('.debug-bar');
 const pulldown = document.querySelector('.pull-down');
+const verElem = document.querySelector('.version');
 
 if (pulldown) {
   pulldown.addEventListener('click', () => {
@@ -40,30 +43,63 @@ const setColor = (prop: string, color: string): void => {
 const updateColorEv = (e: any, prop: string) => {
   const val = e.target.value;
   switch (true) {
-    case val.length < 3: {
-      return;
-    };
     case val.length === 4: {
     }
     case val.length === 7: {
       setColor(prop, e.target.value);
+      break;
     }
+    case val.length < 3: { };
     default: {
       return;
     }
   }
 }
 
-const colorEditor = (prop: string, label: string): HTMLElement => {
+const colorEditor = (prop: string, label: string, element: string): HTMLElement => {
   const customProp = getColor(prop);
 
   const div = document.createElement('div');
   const input = document.createElement('input');
+  input.type = "color";
+  const relatedElement = document.querySelector(`.${element}`)
+
+  if (!relatedElement) {
+    throw new Error('Element could not be bound');
+  }
 
   input.value = customProp;
+
   input.maxLength = 7;
-  input.addEventListener('keyup', e => {
+  input.addEventListener('input', e => {
     updateColorEv(e, prop);
+  });
+
+  input.addEventListener('focusin', (e) => {
+    const ui = document.querySelector('.ui');
+    const bounds = relatedElement.getBoundingClientRect();
+
+    const focDiv = document.createElement('div');
+
+    focDiv.innerHTML = `<div class="focused" id="focused" style="position: fixed; z-index: 25; width: ${bounds.width}px; height: ${bounds.height}px; top: ${bounds.top}px; left: ${bounds.left}px; pointer-events: none;" related="${element}"></div>`;
+
+
+    if (element === 'server-panel') {
+      ui?.classList.add('server-open');
+      setTimeout(() => {
+        document.body.appendChild(focDiv);
+      }, 300);
+    } else {
+      document.body.appendChild(focDiv);
+    }
+  });
+
+  input.addEventListener('focusout', (e) => {
+    const focused = document.querySelector('.focused');
+    focused?.remove();
+    if(element === 'server-panel') {
+      ui?.classList.remove('server-open');
+    }
   });
 
   div.innerHTML = `
@@ -75,11 +111,28 @@ const colorEditor = (prop: string, label: string): HTMLElement => {
 
 window.addEventListener('load', () => {
   if (debugBar) {
-    debugBar.appendChild(colorEditor('--server-bar-bg-color', 'Server BG'));
-    debugBar.appendChild(colorEditor('--group-bar-bg-color', 'Group BG'));
-    debugBar.appendChild(colorEditor('--channel-bar-bg-color', 'Channel BG'));
-    debugBar.appendChild(colorEditor('--main-header-bg-color', 'Header BG'));
-    debugBar.appendChild(colorEditor('--main-content-bg-color', 'Main BG'));
-    debugBar.appendChild(colorEditor('--main-panel-bg-color', 'Panel BG'));
+    debugBar.appendChild(colorEditor('--server-bar-bg-color', 'Server BG', 'server-panel'));
+    debugBar.appendChild(colorEditor('--group-bar-bg-color', 'Group BG', 'group-bar'));
+    debugBar.appendChild(colorEditor('--channel-bar-bg-color', 'Channel BG', 'server-inside'));
+    debugBar.appendChild(colorEditor('--main-header-bg-color', 'Header BG', 'main-panel__header'));
+    debugBar.appendChild(colorEditor('--main-content-bg-color', 'Main BG', 'main-panel__content-primary'));
+    // debugBar.appendChild(colorEditor('--main-panel-bg-color', 'Panel BG'));
   }
-})
+});
+
+window.addEventListener('resize', () => {
+  console.log('resized');
+  const focused = document.getElementById('focused');
+  if (!focused) return;
+  const related = document.querySelector(`.${focused.getAttribute('related')}`);
+  if (!related) return;
+  const bounds = related.getBoundingClientRect();
+  focused.style.top = `${bounds.top}px`;
+  focused.style.left = `${bounds.left}px`;
+  focused.style.height = `${bounds.height}px`;
+  focused.style.width = `${bounds.width}px`;
+});
+
+if(verElem) {
+  verElem.textContent += version;
+}
